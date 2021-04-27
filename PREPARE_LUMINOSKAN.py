48582941f9e30c0d1973f26cbcb3e2dc171baae5
clockgene <49060239+clockgene@users.noreply.github.com>
2021-04-27 12:04:46 +0200

    Update PREPARE_LUMINOSKAN.py

--- PREPARE_LUMINOSKAN.py
+++ PREPARE_LUMINOSKAN.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb  3 13:01:03 2021
-# v2021.04.23
+# v2021.04.27
 @author: Martin.Sladek
 """
 import numpy  as np
 import pandas as pd
 import settings
-import os
+import os, shutil
 
 
 # Choose, which plate - if 96 well, it has 12 columns, if 384 well, it has 24 columns.
-no_of_columns = 12    # comment out (add #) for 384-well plate
-#no_of_columns = 24   # uncomment (delete # on this line) for 384-well plate
+plate = 96    # comment out (add #) for 384-well plate
+#plate = 384   # uncomment (delete # on this line) for 384-well plate
 
 
 # calls global variables from module settings.py, for tkinter file selecting
 settings.init()
-
 # function, i-l are positions of table, modulus sets repeated row
 def split_data(data, i=2, j=10, k=1, l=13, modulus=10, start=2):
 
@@ -51,15 +50,22 @@ except:
 
 
 # Execute function
-if no_of_columns == 12:    
+if plate == 96:    
     df = split_data(data)
-else:    
+if plate == 384:   
     df = split_data(data, i=2, j=18, k=1, l=25, modulus=18, start=2)
 
 # Export dataframe as csv file 
 mydir = './data/analysis_output__/'
 os.makedirs(mydir, exist_ok = True)  #creates analysis_output folder in case it is missing
 df.to_csv(f'{mydir}{settings.INPUT_FILES[0]}_signal.csv', index=False)
+
+# Copy correct XY file to analysis_output__/ and rename it
+template_dir = './_templates/analysis_output__/'
+if plate == 96:
+    shutil.copy(f'{template_dir}96_XY.csv', f'{mydir}{settings.INPUT_FILES[0]}_XY.csv')
+if plate == 384:
+    shutil.copy(f'{template_dir}384_XY.csv', f'{mydir}{settings.INPUT_FILES[0]}_XY.csv')
 
 
 # test data 
